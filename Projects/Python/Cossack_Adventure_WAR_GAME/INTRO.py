ef063e702ed91a6362143ecdc88b139bb7949280
CB4$H <thecybercossack@proton.me>
2025-05-12 12:32:29 -0500

    Create INTRO.py

---
+++ Projects/Python/Cossack_Adventure_WAR_GAME/INTRO.py
@@ -0,0 +1,10 @@
+import random
+
+cossack_names = ["Taras", "Bohdan", "Levko", "Mykhailo", "Yarema", "Stepan", "Vasyl", "Petro"]
+name = random.choice(cossack_names)
+
+hp = 100
+inventory = ["shashka", "apple"]
+
+print("Slava Cossackia!")
+print(f"You are {name} of the Steppe!")
